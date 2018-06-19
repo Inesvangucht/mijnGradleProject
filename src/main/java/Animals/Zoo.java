8e2fe6b770bf2dd962c797793e132cfc81426916
Inés Van Gucht <inesvangucht@gmail.com>
2018-06-19 15:16:26 +0200

    test zoe

--- src/main/java/Animals/Zoo.java
+++ src/main/java/Animals/Zoo.java
@@ -3,9 +3,9 @@ package Animals;
 public class Zoo {
     public static void main(String[] args) {
 
-
-
-
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        }
         Animal catm = new Cat( "Moortje", 9);
         Animal catf = new Cat("Marie-Charlotte", 9);
 
